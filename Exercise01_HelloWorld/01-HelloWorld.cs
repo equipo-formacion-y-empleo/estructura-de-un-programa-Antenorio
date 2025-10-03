2092ee1c6ed38c40eb3c0aa1f67b3056d36b4d78
Antenorio <atenoriom01@educantabria.es>
2025-10-03 19:03:30 +0200

    Update 01-HelloWorld.cs

--- Exercise01_HelloWorld/01-HelloWorld.cs
+++ Exercise01_HelloWorld/01-HelloWorld.cs
@@ -2,6 +2,6 @@ string ShowMessage(string name)
 {
     throw new NotImplementedException();
 }
-
 Console.WriteLine(ShowMessage(args.Length > 0 ? args[0] : "Nombre1"));
 Console.WriteLine(ShowMessage(args.Length > 1 ? args[1] : "Nombre2"));
+Console.WriteLine(ShowMessage(args.Length > 2 ? args[2] : "Nombre3"));
