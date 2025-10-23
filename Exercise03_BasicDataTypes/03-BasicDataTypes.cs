f68ba59bb3636dfacd54844df5fd6b22720235f2
Antenorio <atenoriom01@educantabria.es>
2025-10-23 12:04:00 +0200

    Ejercicio3

--- Exercise03_BasicDataTypes/03-BasicDataTypes.cs
+++ Exercise03_BasicDataTypes/03-BasicDataTypes.cs
@@ -1,12 +1,21 @@
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
 void DemoBasicDataTypes()
 {
-    throw new NotImplementedException();
+    int age = 25;
+    double price = 19.99;
+    char initial = 'A';
+    bool available = true;
+
 }
 
 void ShowDataTypeSizes()
 {
-    throw new NotImplementedException();
-}
+    Console.WriteLine("age = " + age + "(size: " + sizeof(int)+ ")");
+}   Console.WriteLine("price = "+ price + " (size: "+ sizeof(double)+ ")");
+    Console.WriteLine("initial = "+ initial + " (size: "+ sizeof(char) + ")");
+    Console.WriteLine("available = "+ available + "(size: "+ sizeof(bool) + ")");
 
 DemoBasicDataTypes();
 ShowDataTypeSizes();
